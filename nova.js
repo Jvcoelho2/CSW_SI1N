c32e9aa22860302e581c1038a2d8490430b5a8f8
Jorge Vitor Nogueira Coelho <134976524+Jvcoelho2@users.noreply.github.com>
2025-05-20 20:45:48 -0300

    Add files via upload
    teste idade

---
+++ nova.js
@@ -0,0 +1,8 @@
+function identificaridade(){
+    var idade = document.getElementById("idade").value;
+    idade = parseInt(idade)  
+alert (resultado)
+var resultado = "";
+if (idade == 0) {document.getElementById("idade").innerHTML = "recem nascido";}
+
+}
